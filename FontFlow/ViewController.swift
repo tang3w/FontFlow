4160d37e81712c824be50904e49a6e5bbdb06f2b
Tianyong Tang <tang3w@gmail.com>
2026-03-20 20:03:43 +0800

    Init project

---
+++ FontFlow/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FontFlow
+//
+//  Created by Tang Tianyong on 2026/3/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ FontFlowTests/FontFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  FontFlowTests.swift
+//  FontFlowTests
+//
+//  Created by Tang Tianyong on 2026/3/20.
+//
+
+import Testing
+@testable import FontFlow
+
+struct FontFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
